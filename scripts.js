410ccd1bbe76f254bf81351ae2383439f47e5ebb
EduardoMatos08 <matos08edu@gmail.com>
2024-08-05 16:00:25 -0300

    commit - projeto finalizado, página do iPhone 14 do site da Apple

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function changeColor(cor) {
+    const divColor = document.querySelector("#colorBackground")
+    divColor.style.backgroundColor = cor;
+}
+
+function changeImage(image) {
+    const iphoneImg = document.querySelector("#iphone-img")
+    iphoneImg.src = image;
+}
