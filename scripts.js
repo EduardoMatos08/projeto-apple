1b9c5c1c4811fedbabbc7f7c2fab10d0958e9870
EduardoMatos08 <matos08edu@gmail.com>
2024-08-05 16:02:33 -0300

    atualização das funções

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-function changeColor(cor) {
-    const divColor = document.querySelector("#colorBackground")
-    divColor.style.backgroundColor = cor;
-}
 
 function changeImage(image) {
     const iphoneImg = document.querySelector("#iphone-img")
     iphoneImg.src = image;
+}
+
+function changeColor(cor) {
+    const divColor = document.querySelector("#colorBackground")
+    divColor.style.backgroundColor = cor;
 }
