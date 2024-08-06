1048685efcc4d71e87b37420dfb45f9f76a82734
EduardoMatos08 <matos08edu@gmail.com>
2024-08-06 16:25:27 -0300

    Commit de adição de comentários

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 function changeColor(cor, image) {
+    // Troca de Imagem
     const iphoneImg = document.querySelector("#iphone-img")
     iphoneImg.src = image;
-
+    // Troca de Cor de Fundo
     const divColor = document.querySelector("#colorBackground")
     divColor.style.backgroundColor = cor;
 }
