4915f18ed7ccab235d7cdd029dc4e417f9a5aec9
EduardoMatos08 <matos08edu@gmail.com>
2024-08-06 16:21:54 -0300

    alteração js, primeira linha inutilizada

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-
 function changeColor(cor, image) {
     const iphoneImg = document.querySelector("#iphone-img")
     iphoneImg.src = image;
-    
+
     const divColor = document.querySelector("#colorBackground")
     divColor.style.backgroundColor = cor;
 }
